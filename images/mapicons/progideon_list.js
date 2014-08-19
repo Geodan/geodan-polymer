ca397ffcd76165053754cb221434af178618aacd
geodan <geodan@cirrus.(none)>
2014-08-19 13:14:17 +0200

    First commit

---
+++ images/mapicons/progideon_list.js
@@ -0,0 +1,40 @@
+{"icons":[
+{"cat":"none", "text":"ZW", "url":"mapicons/direction_downleft.png"}
+,{"cat":"none", "text":"Z","url":"mapicons/direction_down.png"}
+,{"cat":"none", "text":"ZO","url":"mapicons/direction_downright.png"}
+,{"cat":"none", "text":"O","url":"mapicons/direction_left.png"}
+,{"cat":"none", "text":"W","url":"mapicons/direction_right.png"}
+,{"cat":"none", "text":"NW","url":"mapicons/direction_upleft.png"}
+,{"cat":"none", "text":"N","url":"mapicons/direction_up.png"}
+,{"cat":"none", "text":"NO","url":"mapicons/direction_upright.png"}
+,{"cat":"none", "text":"Afgesloten","url":"mapicons/accesdenied.png"}
+,{"cat":"none", "text":"Let op!","url":"mapicons/caution.png"}
+,{"cat":"none", "text":"Groepsvervoer","url":"mapicons/bus.png"}
+,{"cat":"none", "text":"Parkeerplaats","url":"mapicons/parkinggarage.png"}
+,{"cat":"none", "text":"EHBO","url":"mapicons/firstaid.png"}
+,{"cat":"none", "text":"Noodtelefoon","url":"mapicons/emergencyphone.png"}
+,{"cat":"none", "text":"Ambulancepost","url":"mapicons/ambulance.png"}
+,{"cat":"none", "text":"Ziekenhuis","url":"mapicons/hospital-building.png"}
+,{"cat":"none", "text":"Apotheek","url":"mapicons/medicine.png"}
+,{"cat":"none", "text":"School","url":"mapicons/school.png"}
+,{"cat":"none", "text":"Zaal","url":"mapicons/communitycentre.png"}
+,{"cat":"none", "text":"Verzorgingstehuis","url":"mapicons/seniorsite.png"}
+,{"cat":"none", "text":"none","url":"mapicons/family.png"}
+,{"cat":"none", "text":"Kinderopvang","url":"mapicons/daycare.png"}
+,{"cat":"none", "text":"Calamiteit","url":"mapicons/blast.png"}
+,{"cat":"none", "text":"Dodelijke slachtoffers","url":"mapicons/skull.png"}
+,{"cat":"none", "text":"Schade aan huis","url":"mapicons/earthquake-3.png"}
+,{"cat":"none", "text":"Stroomuitval","url":"mapicons/poweroutage.png"}
+,{"cat":"none", "text":"Brand","url":"mapicons/fire.png"}
+,{"cat":"none", "text":"Overstroming","url":"mapicons/flood.png"}
+,{"cat":"none", "text":"Radio zender","url":"mapicons/radio-station-2.png"}
+,{"cat":"none", "text":"Verzamelpunt","url":"mapicons/regroup.png"}
+,{"cat":"none", "text":"Netwerk","url":"mapicons/wifi.png"}
+,{"cat":"none", "text":"Groep mensen","url":"mapicons/group-2.png"}
+,{"cat":"none", "text":"Brandweer","url":"mapicons/firemen.png"}
+,{"cat":"none", "text":"Politie","url":"mapicons/police.png"}
+,{"cat":"none", "text":"Camera","url":"mapicons/cctv.png"}
+,{"cat":"none", "text":"Gemeentehuis","url":"mapicons/moderntower.png"}
+,{"cat":"none", "text":"Commentaar","url":"mapicons/comment-map-icon.png"}
+,{"cat":"none", "text":"Happy","url":"mapicons/smiley_happy.png"}
+]}      
